612ef44d237d66a67d3d84d55d141e9c7ca48eab
dqhuy0796 <68587628+dqhuy0796@users.noreply.github.com>
2022-12-08 12:57:19 +0700

    update admin ui 1.1

--- src/layouts/AdminMenu/AdminMenu.js
+++ src/layouts/AdminMenu/AdminMenu.js
@@ -1,18 +1,23 @@
 import classNames from 'classnames/bind';
 import React from 'react';
+import { TbTag } from 'react-icons/tb';
+import { MdOutlineFastfood } from 'react-icons/md';
+import { RiUserSettingsLine, RiUserStarLine, RiLogoutBoxRLine } from 'react-icons/ri';
+import { TfiDashboard } from 'react-icons/tfi';
 import { NavLink } from 'react-router-dom';
-import config from '~/config';
 import HamburgerButton from '~/components/shared/HamburgerButton';
+import config from '~/config';
+import LogoFull from '~/components/shared/Logo/LogoFull';
 import styles from './AdminMenu.module.scss';
 
 const cb = classNames.bind(styles);
 
 const menu = [
-    { title: 'bảng điều khiển', path: config.routes.dashboard },
-    { title: 'tài khoản', path: config.routes.users },
-    { title: 'khách hàng', path: config.routes.customers },
-    { title: 'sản phẩm', path: config.routes.products },
-    { title: 'đơn hàng', path: config.routes.orders },
+    { icon: <TfiDashboard />, title: 'bảng điều khiển', path: config.routes.dashboard },
+    { icon: <RiUserSettingsLine />, title: 'tài khoản', path: config.routes.users },
+    { icon: <RiUserStarLine />, title: 'khách hàng', path: config.routes.customers },
+    { icon: <MdOutlineFastfood />, title: 'sản phẩm', path: config.routes.products },
+    { icon: <TbTag />, title: 'đơn hàng', path: config.routes.orders },
 ];
 
 class AdminMenu extends React.Component {
@@ -31,20 +36,21 @@ class AdminMenu extends React.Component {
         return (
             <div className={cb('menu', this.state.isMobileMenuOpening && 'collapse')}>
                 <div className={cb('navbar')}>
-                    <div className={cb('hamburger-btn')}>
-                        <HamburgerButton
-                            onClick={this.handleCollapseMenu}
-                            isCollapsed={this.state.isMobileMenuOpening}
-                        />
-                    </div>
                     <ul>
+                        <li>
+                            <HamburgerButton
+                                onClick={this.handleCollapseMenu}
+                                isCollapsed={this.state.isMobileMenuOpening}
+                            />
+                            <span className={cb('logo')}>MMFood</span>
+                        </li>
                         {menu.map((item, index) => (
                             <li key={index}>
-                                <MenuItem path={item.path} title={item.title} />
+                                <MenuItem path={item.path} icon={item.icon} title={item.title} />
                             </li>
                         ))}
                         <li>
-                            <MenuItem path={config.routes.login} title="đăng xuất" />
+                            <MenuItem path={config.routes.login} icon={<RiLogoutBoxRLine />} title="đăng xuất" />
                         </li>
                     </ul>
                 </div>
@@ -54,14 +60,10 @@ class AdminMenu extends React.Component {
 }
 
 const MenuItem = (props) => (
-    <div className={cb('menu-item')}>
-        <NavLink
-            to={props.path}
-            className={({ isActive }) => (isActive ? cb('menu-link', 'actived') : cb('menu-link'))}
-        >
-            <span>{props.title}</span>
-        </NavLink>
-    </div>
+    <NavLink to={props.path} className={({ isActive }) => (isActive ? cb('menu-link', 'actived') : cb('menu-link'))}>
+        <span className={cb('icon')}>{props.icon}</span>
+        <span className={cb('title')}>{props.title}</span>
+    </NavLink>
 );
 
 export default AdminMenu;
--- src/pages/admin/Users/Users.js
+++ src/pages/admin/Users/Users.js
@@ -4,8 +4,8 @@ import styles from './Users.module.scss';
 import AdminMenu from '~/layouts/AdminMenu/AdminMenu';
 import PillSmallButton from '../../../components/shared/PillSmallButton';
 import RealtimeClock from '~/components/partial/RealtimeClock';
-import { BiUserPlus, BiUpload, BiPrinter, BiSpreadsheet } from 'react-icons/bi';
-import { GrDocumentPdf } from 'react-icons/gr';
+import { BiPlus, BiUpload, BiPrinter, BiSpreadsheet } from 'react-icons/bi';
+import { VscFilePdf } from 'react-icons/vsc';
 import { RiDeleteBin5Line } from 'react-icons/ri';
 
 const cb = classNames.bind(styles);
@@ -15,7 +15,7 @@ class Users extends React.Component {
         menu: [
             {
                 id: 1,
-                icon: <BiUserPlus />,
+                icon: <BiPlus />,
                 title: 'Thêm mới',
             },
             {
@@ -35,13 +35,45 @@ class Users extends React.Component {
             },
             {
                 id: 5,
-                icon: <GrDocumentPdf />,
+                icon: <VscFilePdf />,
                 title: 'Xuất PDF',
             },
             {
                 id: 6,
                 icon: <RiDeleteBin5Line />,
                 title: 'Xóa tất cả',
+            },
+        ],
+        dataTable: [
+            {
+                id: 1,
+                name: 'Nguyễn Thị Hoài',
+                birth: '21/08/2001',
+                avatarUrl: '',
+                phone: '0989.989.989',
+                email: 'example@email.com',
+                address: 'Hiệp Hòa - Bắc Giang',
+                role: 'admin',
+            },
+            {
+                id: 2,
+                name: 'Nguyễn Thị Hâm',
+                birth: '21/08/2001',
+                avatarUrl: '',
+                phone: '0989.989.989',
+                email: 'example@email.com',
+                address: 'Hiệp Hòa - Bắc Giang',
+                role: 'admin',
+            },
+            {
+                id: 3,
+                name: 'Nguyễn Thị Hấp',
+                birth: '21/08/2001',
+                avatarUrl: '',
+                phone: '0989.989.989',
+                email: 'example@email.com',
+                address: 'Hiệp Hòa - Bắc Giang',
+                role: 'admin',
             },
         ],
     };
@@ -66,7 +98,30 @@ class Users extends React.Component {
                                 ))}
                             </ul>
                         </div>
-                        <div className={cb('body')}></div>
+                        <div className={cb('body')}>
+                            <table cellPadding="0" cellSpacing="0" border="0" className={cb('main-table')}>
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <input type={'checkbox'} />
+                                        </th>
+                                        <th>id</th>
+                                        <th>Họ và tên</th>
+                                        <th>Ngày sinh</th>
+                                        <th>Avatar</th>
+                                        <th>SĐT</th>
+                                        <th>Email</th>
+                                        <th>Địa chỉ</th>
+                                        <th>Chức vụ</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.dataTable.map((item, index) => (
+                                        <DataTableRow key={index} data={item} />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                         <div className={cb('footer')}></div>
                     </div>
                 </div>
@@ -76,3 +131,19 @@ class Users extends React.Component {
 }
 
 export default Users;
+
+const DataTableRow = (props) => (
+    <tr>
+        <td>
+            <input type={'checkbox'} />
+        </td>
+        <td>{props.data.id}</td>
+        <td>{props.data.name}</td>
+        <td>{props.data.birth}</td>
+        <td>{props.data.avatarUrl}</td>
+        <td>{props.data.phone}</td>
+        <td>{props.data.email}</td>
+        <td>{props.data.address}</td>
+        <td>{props.data.role}</td>
+    </tr>
+);
